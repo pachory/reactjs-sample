ccf7ef83a441718975248a86faa31b233952f34a
shigematsu <shigematsu@snowrobin.jp>
2018-10-10 14:01:26 +0900

    gatsby-book P22 まで進めた

---
+++ src/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <h1 className="header-title">ヘッダー</h1>
+            <nav className="headerNav">
+                <a className="headerNav-item" href="#about">About</a>
+                <a className="headerNav-item" href="#works">Works</a>
+            </nav>
+        </header>
+    )
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./header";
+import Footer from "./footer";
+import "minireset.css";
+import "./styles.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+    return (
+        <div className="App">
+            <Header/>
+            <div className="hero">
+                <h1 className="hero-title">タイトル</h1>
+                <p className="hero-description">サブタイトル</p>
+            </div>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+            <section className="section" id="about">
+                <h1 className="section-title">About セクション タイトル</h1>
+                <p className="aboutDescription">about セクション 紹介文</p>
+            </section>
+
+            <section className="section" id="works">
+                <h1 className="section-title">Works セクション タイトル</h1>
+                <ul>
+                    <li className="workItem">
+                        <img className="workItem-image" src="https://dummyimage.com/600x300/eee/ccc"/>
+                        <h3 className="workItem-category">カテゴリー名</h3>
+                        <h3 className="workItem-title">タイトル</h3>
+                    </li>
+                    <li className="workItem">
+                        <img className="workItem-image" src="https://dummyimage.com/600x300/eee/ccc"/>
+                        <h3 className="workItem-category">カテゴリー名</h3>
+                        <h3 className="workItem-title">タイトル</h3>
+                    </li>
+                </ul>
+            </section>
+
+            <Footer/>
+        </div>
+    )
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App/>, rootElement);
